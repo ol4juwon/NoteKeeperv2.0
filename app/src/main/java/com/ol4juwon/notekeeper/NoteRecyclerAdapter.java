eb0d33c0ca0a845c56ad2f8edbb5ed88f952920c
ol4 juwon <olajuwonlawal@icloud.com>
2020-08-24 23:06:20 +0100

    recycler view wip

--- app/src/main/java/com/ol4juwon/notekeeper/NoteRecyclerAdapter.java
+++ app/src/main/java/com/ol4juwon/notekeeper/NoteRecyclerAdapter.java
@@ -1,8 +1,10 @@
 package com.ol4juwon.notekeeper;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,16 +12,19 @@ import androidx.recyclerview.widget.RecyclerView;
 public class NoteRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private final Context mContext;
+    private final LayoutInflater mLayoutInflater;
 
     public NoteRecyclerAdapter(Context context) {
         mContext = context;
+        mLayoutInflater = LayoutInflater.from(mContext);
     }
 
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = mLayoutInflater.inflate(R.layout.item_note_list,parent,false);
+        return new ViewHolder(itemView);
     }
 
     @Override
@@ -34,8 +39,15 @@ public class NoteRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        public final TextView mTextCourse;
+        public final TextView mTextTitle;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            mTextCourse = (TextView) itemView.findViewById(R.id.text_course);
+            mTextTitle = (TextView) itemView.findViewById(R.id.text_title);
+
+
         }
     }
 }
