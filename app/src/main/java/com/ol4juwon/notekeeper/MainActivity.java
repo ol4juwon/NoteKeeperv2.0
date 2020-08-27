49dc4493140d25ab8fe53b958e1fcb5503b801ff
ol4 juwon <olajuwonlawal@icloud.com>
2020-08-27 15:20:12 +0100

    previous note option menu item added

--- app/src/main/java/com/ol4juwon/notekeeper/MainActivity.java
+++ app/src/main/java/com/ol4juwon/notekeeper/MainActivity.java
@@ -2,55 +2,80 @@ package com.ol4juwon.notekeeper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
 
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.View;
-
 import java.util.List;
 
-public class NoteListActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     private NoteRecyclerAdapter mNoteRecyclerAdapter;
-
-//    private ArrayAdapter<NoteInfo> mAdapterNotes;
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_note_list);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               startActivity(new Intent(NoteListActivity.this,NoteActivity.class));
+                startActivity(new Intent(MainActivity.this,NoteActivity.class));
             }
         });
-
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
         initializeDisplayContent();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-      //  mAdapterNotes.notifyDataSetChanged();
+        //  mAdapterNotes.notifyDataSetChanged();
         mNoteRecyclerAdapter.notifyDataSetChanged();
-    // this let's the recycler view know that the data set has changed and to be updated as required.
+        // this let's the recycler view know that the data set has changed and to be updated as required.
 
     }
 
     private void initializeDisplayContent() {
 
         //create a variable for list notes
-        final RecyclerView recyclerNotes = (RecyclerView) findViewById(R.id.list_notes);
+        final RecyclerView recyclerNotes = (RecyclerView) findViewById(R.id.list_items);
 //        create a new layout manager to manager the recycler view
         final LinearLayoutManager notesLayoutManager = new LinearLayoutManager(this);
 //        set the recycler view to use the new layout manager
@@ -87,5 +112,17 @@ public class NoteListActivity extends AppCompatActivity {
 //        });
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
 }
